dc96f5b258bf698b606133ee493cbc356235de2c
MashiBan <kb00664422@gmail.com>
2026-09-02 23:06:42 +0530

    app completed

--- packages/db/index.js
+++ packages/db/index.js
@@ -1,1 +1,3 @@
-export { client } from './src/index.js';
+export { client } from './dist/index.js';
+
+
--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -1,4 +1,2 @@
-import {PrismaClient} from "@prisma/client"
-
-export const client = new PrismaClient()
-
+import { PrismaClient } from "@prisma/client";
+export const client = new PrismaClient();
