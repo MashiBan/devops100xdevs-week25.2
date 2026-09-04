3f452c728e9efefdfd9732f16264ff0023a35e07
MashiBan <kb00664422@gmail.com>
2026-09-05 02:58:17 +0530

    edited page.ts in web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <div>
-      First name haha: 
+      Tell us ur First name: 
       {user?.username}
       password: 
       {user?.password}
